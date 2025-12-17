f30a70ef08132fa0cdeb80558833241abd55d59e
Pat <patrickross216@gmail.com>
2025-12-18 00:58:00 +1100

    Restructures script to be more modular

--- noYoutubeAiSummary.js
+++ noYoutubeAiSummary.js
@@ -1,17 +1,26 @@
 /**
  * Observer function that triggers a callback for a set of elements whenever the DOM is updated.
- * @param {string} selector 
+ * @param {string[]} selectors
  * @param {elementCallback} callback 
  */
-function waitForElements(selector, callback) {
+function waitForElements(selectors, callback) {
     /**
-     * Retrieves a list of elements that match the selector in waitForElements() 
+     * Retrieves a list of elements that match all selectors in waitForElements() 
      * and executes a callback function if any exists.
      */
     const hideElements = () => {
-        const elementList = document.querySelectorAll(selector);
+        // For each selector, append all matching elements into one array 
+        let elementList = [];
+        selectors.forEach(selector => {
+            selectorElements = document.querySelectorAll(selector);
+            selectorElementsArray = Array.from(selectorElements);
+            elementList = elementList.concat(selectorElementsArray);
+        });
+
         if (elementList.length > 0) {
+            // console.log(`[No YouTube AI Summary] ${elementList.length} Elements to hide!`);
             callback(elementList);
+            // console.log(`[No YouTube AI Summary] Callback done!`);
         }
     };
 
@@ -31,25 +40,26 @@ function waitForElements(selector, callback) {
 /* 
  * Execution starts here 
  */
+console.log("[No YouTube AI Summary] Loading...");
+
+// These are the elements to be hidden.
+// Add more or tune to your liking!
+const blockedSelectors = [
+    '#expandable-metadata', // Used below description (watch), beside video (results)
+    '.expandableMetadataRendererHost', // Used in description (m.youtube.com/watch)
+    '#video-summary' // Used in description (watch)
+];
+
+waitForElements(blockedSelectors, (elements) => {
+    // console.log(`[No YouTube AI Summary] In callback! Elements are: ${elements}`);
+    elements.forEach(el => {
+        el.style.display = "none";
+    });
+    // Fair Warning: This console.log will rapidly fill your console every time the DOM is updated!
+    // console.log(`[No YouTube AI Summary] Summary Blocked!`);
+})
+
 console.log("[No YouTube AI Summary] Loaded!");
-
-waitForElements('#expandable-metadata', (elements) => {
-    elements.forEach(el => {
-        el.style.display = "none";
-    });
-    // Fair Warning: This console.log will rapidly fill your console every time the DOM is updated!
-    // console.log(`[No YouTube AI Summary] Summary Blocked!`);
-})
-
-// For the descriptions in m.youtube.com/watch (they use a class instead)
-waitForElements('.expandableMetadataRendererHost', (elements) => {
-    elements.forEach(el => {
-        el.style.display = "none";
-    });
-    // Fair Warning: This console.log will rapidly fill your console every time the DOM is updated!
-    // console.log(`[No YouTube AI Summary] Summary Blocked!`);
-})
-
 
 /**
  * @callback elementCallback
